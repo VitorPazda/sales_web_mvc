da38e2d222d874375d004965298a89591020ed7d
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-13 21:59:15 -0300

    Adicionando os arquivos

---
+++ sales_web_mvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sales_web_mvc.Models;
+
+namespace sales_web_mvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Nome = "Eletronicos" });
+            list.Add(new Department { Id = 2, Nome = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ sales_web_mvc/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using sales_web_mvc.Models.ViewModels;
+
+namespace sales_web_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Message"] = "A descricao da sua aplicacao";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
