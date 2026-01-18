0d18a9d794289689eddf457afbaef01686a77297
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-18 17:28:30 -0300

    atualizando as entidades do programa

--- sales_web_mvc/Migrations/20260118202723_OtherEntities.Designer.cs
+++ sales_web_mvc/Migrations/20260118202723_OtherEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sales_web_mvc.Data;
 
@@ -11,9 +12,11 @@ using sales_web_mvc.Data;
 namespace sales_web_mvc.Migrations
 {
     [DbContext(typeof(sales_web_mvcContext))]
-    partial class sales_web_mvcContextModelSnapshot : ModelSnapshot
+    [Migration("20260118202723_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- sales_web_mvc/Models/Department.cs
+++ sales_web_mvc/Models/Department.cs
@@ -4,5 +4,26 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); // Departamos possui vendedores, entao usamos um ICollection
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
