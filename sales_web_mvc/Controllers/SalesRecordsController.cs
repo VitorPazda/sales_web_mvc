45f297d70190c4c31507de757971f7b9467a15b6
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-26 21:31:52 -0300

    criando o controller de salesrecords junto com as views de grouping e simple search

---
+++ sales_web_mvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sales_web_mvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
